54e4151d21af0a7825131affdcaaa39e6be2ac7c
o-kollar <105907960+o-kollar@users.noreply.github.com>
2023-01-05 18:55:16 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,100 +1,74 @@
-let fromLatitude;
-let fromLongitude;
-const suggestedLocation = document.getElementById('suggestedLocation');
-const suggestedDestination = document.getElementById('suggestedDestination');
+<!doctype html>
+<html>
+<head>
+  <title>API Test</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="./common.css" rel="stylesheet">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <iframe  class="position-absolute mt-10 z-1" id="map-iframe" src="https://www.freemap.sk/#map=1/48.687334/19.500732&layers=O&embed=noMapSwitch,noLocateMe" style="width: 100%; height: 500px; border: 0; " allowfullscreen></iframe>
+  <div class="w-full mx-auto  position-absolute top-10 z-2">
+    <div class="bg-white shadow-md rounded-lg px-3 py-2 mb-4">
 
-document.getElementById('useLocation').addEventListener('click', () => {
-    // check if the browser supports geolocation
-    if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            // get latitude and longitude
-            fromLatitude = position.coords.latitude;
-            fromLongitude = position.coords.longitude;
-            // set text of p element to latitude and longitude
-            drawMap(fromLatitude, fromLongitude);
-            document.getElementById('locationInput').value = 'Moja poloha';
-        });
-    }
-});
+        <div class=" w-80 flex items-center bg-gray-200 rounded-md">
+            <div class="pl-2">
+                <svg class="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24">
+                    <path class="heroicon-ui"
+                        d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                </svg>
+            </div>
+            <input
+                class="w-80 rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
+                id="locationInput" type="text" placeholder="Search teams or members">
+        </div>
+        <button id="useLocation" type="button" class="btn btn-light" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom"><span class="material-symbols-outlined">
+          share_location
+          </span></button>
+        <div id="suggestedLocation" class="py-3 text-sm">
+          
+           
+           
+        </div>
+       
+    </div>
+</div>
+  
+      </div>
+      </div>
 
-document.getElementById('locationInput').addEventListener('input', () => {
-    searchAddresses('from', document.getElementById('locationInput').value);
-});
-document.getElementById('destinationInput').addEventListener('input', () => {
-    searchAddresses('to', document.getElementById('destinationInput').value);
-});
-function drawMap(lat, lng) {
-    const src = `https://www.freemap.sk/#map=13/${lat}/${lng}&layers=O&point=${lat}/${lng}%1EC%23ff6251&embed=noMapSwitch,noLocateMe`;
-    document.getElementById('map-iframe').src = src;
-}
+      
+<div class="offcanvas offcanvas-bottom big-offcanvas" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel" data-bs-scroll="true" data-bs-backdrop="false">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasBottomLabel">Offcanvas bottom</h5>
+    
+    <ul id="address-list" class="list-group">
+  
+    <p id="element-id"></p>
+  
+</div>
+<div class="offcanvas-body small">
+  <div class="dropdown">
+<input type="text" id="destinationInput" class="form-control dropdown-toggle"  data-bs-toggle="dropdown" aria-expanded="false">
+<ul id="suggestedDestination" class="dropdown-menu">
+</ul>
+<ul class="list-group">
 
-function searchAddresses(direction, filter) {
-    if (direction === 'from') {
-        suggestedLocation.innerHTML = '';
-    } else if (direction === 'to') {
-        suggestedDestination.innerHTML = '';
-    }
+</ul>
+</div>
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://clientapi.dopravnakarta.sk/api/v2/GetLocationsByAddressFilter');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('X-API-Key', '00112233445566778899');
-    xhr.setRequestHeader('X-App-Version', '1');
-    xhr.onload = () => {
-        const response = JSON.parse(xhr.responseText);
-        const addresses = response.AddressesLocations;
-        updateSuggestedLocations(direction, addresses);
-    };
-    xhr.send(
-        JSON.stringify({
-            filter: filter,
-            maxCount: 64,
-            latitude: 3.1,
-            longitude: 4.1,
-            stopTypes: 64,
-            cityID: 0,
-        })
-    );
-}
+ 
+ 
+  
 
-
-function updateSuggestedLocations(direction, addresses) {
-    if (direction === 'from') {
-        addresses.forEach((address) => {
-            const div = document.createElement('div');
-            div.classList.add( 'text-sm');
-            div.innerHTML = `
-                <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
-                    <span class="bg-gray-400 rounded-full"></span>
-                    <div class="flex-grow font-medium px-2">${address.LocationName}</div>
-                </div>
-            `;
-            suggestedLocation.appendChild(div);
-            div.addEventListener('click', () => {
-                document.getElementById('locationInput').value = address.LocationName;
-                fromLatitude = address.Latitude;
-                fromLongitude = address.Longitude;
-                suggestedLocation.innerHTML="";
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="./javascript.js"></script>
+<script src="./controller.js"></script>
 
 
-                drawMap(fromLatitude, fromLongitude);
-            });
-        });
-    }
-    else if (direction === 'to') {
-        addresses.forEach((address) => {
-            const div = document.createElement('div');
-            div.classList.add('py-3', 'text-sm');
-            div.innerHTML = `
-                <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
-                    <span class="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
-                    <div class="flex-grow font-medium px-2">${address.LocationName}</div>
-                </div>
-            `;
-            suggestedDestination.appendChild(div);
-            div.addEventListener('click', () => {
-                document.getElementById('destinationInput').value = address.LocationName;
-            });
-        });
-    }
-}
+</body>
+</html>
+
